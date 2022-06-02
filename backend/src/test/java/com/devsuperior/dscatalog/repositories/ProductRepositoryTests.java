08fc6bfd069b796299fa0d5a81fdeaef393852d5
Ede <edenilsonfc@gmail.com>
2022-06-02 09:54:01 -0300

    Testing repository deleting

---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.devsuperior.dscatalog.entities.Product;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {
+		
+		long existingId = 1L;
+		productRepository.deleteById(existingId);
+
+		Optional<Product> result = productRepository.findById(existingId);
+		
+		Assertions.assertFalse(result.isPresent());
+		
+		
+	}
+
+}
